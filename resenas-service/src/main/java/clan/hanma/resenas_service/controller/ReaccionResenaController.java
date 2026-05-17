ebbf55307d62b82bebd3112227d320a8fe43f9b7
Russentt <ignacio.battistoni.mestre@gmail.com>
2026-05-17 14:12:43 -0400

    Agregar controllers de resenas-service

--- resenas-service/src/main/java/clan/hanma/resenas_service/controller/ReaccionResenaController.java
+++ resenas-service/src/main/java/clan/hanma/resenas_service/controller/ReaccionResenaController.java
@@ -1,10 +1,20 @@
 package clan.hanma.resenas_service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import clan.hanma.resenas_service.model.ReaccionResena;
 import clan.hanma.resenas_service.service.ReaccionResenaService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/reacciones")
@@ -12,4 +22,31 @@ public class ReaccionResenaController {
 
     @Autowired
     private ReaccionResenaService reaccionResenaService;
+
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        return ResponseEntity.ok(reaccionResenaService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(reaccionResenaService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@Valid @RequestBody ReaccionResena rr) {
+        return new ResponseEntity<>(reaccionResenaService.save(rr), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        reaccionResenaService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @Valid @RequestBody ReaccionResena rr) {
+        return ResponseEntity.ok(reaccionResenaService.save(rr));
+    }
+
 }
--- resenas-service/src/main/java/clan/hanma/resenas_service/controller/ResenaController.java
+++ resenas-service/src/main/java/clan/hanma/resenas_service/controller/ResenaController.java
@@ -1,5 +1,53 @@
 package clan.hanma.resenas_service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import clan.hanma.resenas_service.model.Resena;
+import clan.hanma.resenas_service.service.ResenaService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/resenas")
 public class ResenaController {
+
+    @Autowired
+    private ResenaService resenaService;
+
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        return ResponseEntity.ok(resenaService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(resenaService.findById(id));
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> save(@Valid @RequestBody Resena r) {
+        return new ResponseEntity<>(resenaService.save(r), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        resenaService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Resena r) {
+        return ResponseEntity.ok(resenaService.update(id, r));
+    }
+
 
 }
