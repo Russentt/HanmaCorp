793d1051e3c88441234105adbda4defc76e46c3b
Russentt <ignacio.battistoni.mestre@gmail.com>
2026-05-16 20:16:47 -0400

    Concluir creaciones de servicios en ordenes-service

--- ordenes-service/src/main/java/clan/hanma/ordenes_service/service/DetalleOrdenService.java
+++ ordenes-service/src/main/java/clan/hanma/ordenes_service/service/DetalleOrdenService.java
@@ -1,8 +1,11 @@
 package clan.hanma.ordenes_service.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import clan.hanma.ordenes_service.model.DetalleOrden;
 import clan.hanma.ordenes_service.repository.DetalleRepository;
 
 @Service
@@ -10,4 +13,23 @@ public class DetalleOrdenService {
 
     @Autowired
     private DetalleRepository detalleRepository;
+
+    public List<DetalleOrden> findAll() {
+        return detalleRepository.findAll();
+    }
+
+    public DetalleOrden findById(Long id) {
+        return detalleRepository.findById(id).orElse(null);
+    }
+
+    public DetalleOrden save(DetalleOrden detalle) {
+        return detalleRepository.save(detalle);
+    }
+
+    public void delete(Long id) {
+        detalleRepository.deleteById(id);
+    }
+
+    // No se juzga relevante la implementacion del metodo update para la tabla DetalleOrden.
+
 }
--- ordenes-service/src/main/java/clan/hanma/ordenes_service/service/EstadoOrdenService.java
+++ ordenes-service/src/main/java/clan/hanma/ordenes_service/service/EstadoOrdenService.java
@@ -1,8 +1,11 @@
 package clan.hanma.ordenes_service.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import clan.hanma.ordenes_service.model.EstadoOrden;
 import clan.hanma.ordenes_service.repository.EstadoOrdenRepository;
 
 @Service
@@ -10,4 +13,28 @@ public class EstadoOrdenService {
 
     @Autowired
     private EstadoOrdenRepository estadoOrdenRepository;
+
+    public List<EstadoOrden> findAll() {
+        return estadoOrdenRepository.findAll();
+    }
+
+    public EstadoOrden findById(Long id) {
+        return estadoOrdenRepository.findById(id).orElse(null);
+    }
+
+    public EstadoOrden save(EstadoOrden eo) {
+        return estadoOrdenRepository.save(eo);
+    }
+
+    public void delete(Long id) {
+        estadoOrdenRepository.deleteById(id);
+    }
+
+    public EstadoOrden update(Long id, EstadoOrden eo) {
+        EstadoOrden estado = estadoOrdenRepository.findById(id).orElse(null);
+        estado.setNombre(eo.getNombre());
+        estadoOrdenRepository.save(estado);
+        return estado;
+    }
+
 }
