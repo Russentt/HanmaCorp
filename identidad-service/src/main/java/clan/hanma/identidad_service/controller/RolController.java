4c5ebfff1f34453f3e06011771540cca7db04133
Russentt <ignacio.battistoni.mestre@gmail.com>
2026-06-09 08:23:39 -0400

    Completar documentacion preliminar de identidad-service

--- identidad-service/src/main/java/clan/hanma/identidad_service/controller/RolController.java
+++ identidad-service/src/main/java/clan/hanma/identidad_service/controller/RolController.java
@@ -11,8 +11,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import clan.hanma.identidad_service.exception.BadRequestException;
+import clan.hanma.identidad_service.exception.ResourceNotFoundException;
 import clan.hanma.identidad_service.model.Rol;
 import clan.hanma.identidad_service.service.RolService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -28,22 +34,73 @@ public class RolController {
         return ResponseEntity.ok(rolService.findAll());
     }
 
+    @Operation(
+        summary = "Encontrar rol por ID",
+        description = "Metodo que devuelve un rol segun el ID ingresado"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "Rol encontrado con exito",
+        content = @Content(schema = @Schema(implementation = Rol.class))
+    )
+    @ApiResponse(
+        responseCode = "404",
+        description = "Rol no encontrado",
+        content = @Content(schema = @Schema(implementation = ResourceNotFoundException.class))
+    )
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id) {
         return ResponseEntity.ok(rolService.findById(id));
     }
 
+    @Operation(
+        summary = "Guardar rol",
+        description = "Metodo que guarda un rol. Exige un @RequestBody"
+    )
+    @ApiResponse(
+        responseCode = "204",
+        description = "Rol creado con exito",
+        content = @Content(schema = @Schema(implementation = Rol.class))
+    )
+    @ApiResponse(
+        responseCode = "400",
+        description = "Error al crear rol",
+        content = @Content(schema = @Schema(implementation = BadRequestException.class))
+    )
     @PostMapping
     public ResponseEntity<?> save(@RequestBody Rol rol) {
         return new ResponseEntity<>(rolService.save(rol), HttpStatus.CREATED);
     }
 
+    @Operation(
+        summary = "Eliminar rol por ID",
+        description = "Metodo que elimina un rol segun su id"
+    )
+    @ApiResponse(
+        responseCode = "201",
+        description = "Rol eliminado con exito",
+        content = @Content(schema = @Schema(implementation = Void.class))
+    )
+    @ApiResponse(
+        responseCode = "404",
+        description = "Rol no encontrado",
+        content = @Content(schema = @Schema(implementation = ResourceNotFoundException.class))
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         rolService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+        summary = "Actualizar rol por ID",
+        description = "Metodo que actualiza un rol"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "Rol actualizado con exito",
+        content = @Content(schema = @Schema(implementation = Rol.class))
+    )
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Rol r) {
         return new ResponseEntity<>(rolService.update(id, r), HttpStatus.CREATED);
--- identidad-service/src/main/java/clan/hanma/identidad_service/controller/UsuarioController.java
+++ identidad-service/src/main/java/clan/hanma/identidad_service/controller/UsuarioController.java
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import clan.hanma.identidad_service.dto.UsuarioDTO;
 import clan.hanma.identidad_service.exception.BadRequestException;
 import clan.hanma.identidad_service.exception.ResourceNotFoundException;
 import clan.hanma.identidad_service.model.Usuario;
 import clan.hanma.identidad_service.service.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -38,12 +40,12 @@ public class UsuarioController {
 
     @Operation(
         summary = "Buscar por ID.",
-        description = "Metodo que busca a un usuario segun su id (Long) "
+        description = "Metodo que busca a un usuario segun su id (Long)"
     )
     @ApiResponse(
         responseCode = "200",
         description = "Usuario encontrado",
-        content = @Content(schema = @Schema(implementation = Usuario.class))
+        content = @Content(schema = @Schema(implementation = UsuarioDTO.class))
     )
     @ApiResponse(
         responseCode = "404",
@@ -74,27 +76,97 @@ public class UsuarioController {
         return new ResponseEntity<>(usuarioService.save(u), HttpStatus.CREATED);
     }
 
+    @Operation(
+        summary = "Eliminar por ID",
+        description = "Metodo que elimina a un usuario segun su ID"
+    )
+    @ApiResponse(
+        responseCode = "201",
+        description = "Usuario eliminado con exito",
+        content = @Content(schema = @Schema(implementation = Void.class))
+    )
+    @ApiResponse(
+        responseCode = "404",
+        description = "Usuario no encontrado",
+        content = @Content(schema = @Schema(implementation = ResourceNotFoundException.class))
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         usuarioService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+        summary = "Actualizar usuario",
+        description = "Metodo que actualiza a un usuario si su ID se ha indicado"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "Usuario actualizado",
+        content = @Content(schema = @Schema(implementation = Usuario.class))
+    )
+    @ApiResponse(
+        responseCode = "404",
+        description = "Usuario no encontrado",
+        content = @Content(schema = @Schema(implementation = ResourceNotFoundException.class))
+    )
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable Long id, @Valid @RequestBody Usuario u) {
         return ResponseEntity.ok(usuarioService.update(id, u));
     }
 
+    @Operation(
+        summary = "Encontrar usuario por email",
+        description = "Metodo que encuentra a un usuario segun su email"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "Usuario encontrado",
+        content = @Content(schema = @Schema(implementation = Usuario.class))
+    )
+    @ApiResponse(
+        responseCode = "404",
+        description = "Usuario no encontrado",
+        content = @Content(schema = @Schema(implementation = ResourceNotFoundException.class))
+    )
     @GetMapping("/email/{email}")
     public ResponseEntity<?> findByEmail(@PathVariable String email) {
         return ResponseEntity.ok(usuarioService.findByEmail(email));
     }
 
+    @Operation(
+        summary = "Encontrar usuarios por rol",
+        description = "Metodo que devuelve una lista de usuarios que compartan rol"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "Rol y usuarios encontrados",
+        content = @Content(array = @ArraySchema(schema = @Schema(implementation = Usuario.class)))
+    )
+    @ApiResponse(
+        responseCode = "404",
+        description = "Rol no encontrado",
+        content = @Content(schema = @Schema(implementation = ResourceNotFoundException.class))
+    )
     @GetMapping("/rol/{id}")
     public ResponseEntity<?> findByRol(@PathVariable Long id) {
         return ResponseEntity.ok(usuarioService.findByRol(id));
     }
 
+    @Operation(
+        summary = "Verificar si e-mail esta registrado",
+        description = "Metodo que comprueba si figura como registrado un e-mail dado"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "Se encuentra el e-mail registrado",
+        content = @Content(schema = @Schema(implementation = Boolean.class))
+    )
+    @ApiResponse(
+        responseCode = "404",
+        description = "No se encuentra registrado el e-mail",
+        content = @Content(schema = @Schema(implementation = ResourceNotFoundException.class))
+    )
     @GetMapping("/existe-email/{email}")
     public ResponseEntity<?> emailExists(@PathVariable String email) {
         if (usuarioService.emailExists(email) != false) return ResponseEntity.ok("Se encuentra registrado el email: " + email);
