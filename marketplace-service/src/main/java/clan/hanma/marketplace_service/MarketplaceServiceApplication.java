67b14341e52f8240a2e3d6ec1b00828e2e436180
PAAWSA02LC1331 <ignacio.battistoni.mestre@gmail.com>
2026-06-19 16:08:38 -0400

    Agregar informacion de contacto a marketplace-service

--- marketplace-service/src/main/java/clan/hanma/marketplace_service/MarketplaceServiceApplication.java
+++ marketplace-service/src/main/java/clan/hanma/marketplace_service/MarketplaceServiceApplication.java
@@ -1,11 +1,25 @@
 package clan.hanma.marketplace_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API de Marketplace",
+				version = "1.0.1",
+				description = "Documentacion de API de Marketplace con todos los endpoints creados",
+				contact = @Contact(
+						name= "Ignacio Battistoni Mestre",
+						email= "ig.battistoni@duocuc.cl"
+				)
+		)
+)
 public class MarketplaceServiceApplication {
 
 	public static void main(String[] args) {
